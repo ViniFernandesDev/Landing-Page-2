0c70fc3855667a4f0080c69abc54f31792590391
ViniFernandesDev <vinifernandesdeveloper@gmail.com>
2022-06-17 18:26:37 -0300

    Criação de arquivos base e exportação de imagens

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">    
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="theme-color" content="#6D68AD"/>
+    <meta name="robots" content="index, follow">
+    <link rel="icon" href="img/favicon.png" type="image/x-icon">
+    
+    <meta name="description" content="Lorem Ipsum"/>
+    <meta name="keywords" content="Lorem Ipsum"/>
+
+    <title>Landing Page 2</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<header>
+
+</header>
+
+<main>
+	
+</main>
+
+<footer>
+
+</footer>
+
+<script src="js/jquery.min.js"></script>
+
+</body>
+</html>
